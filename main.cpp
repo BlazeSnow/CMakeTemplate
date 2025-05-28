dfe4ed8cfeee44f4d51a4cdea7a4e6a61a876726
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-05-28 15:56:28 +0800

    filesystem

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <filesystem>
+#include <iostream>
+#include "src/filesystem.h"
+
+using namespace std;
+
+int main() {
+    printf("Hello world!\n");
+    example2();
+    return 0;
+}
---
+++ src/filesystem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <filesystem>
+#include "filesystem.h"
+
+using namespace std;
+
+const filesystem::path CurrentPath = filesystem::current_path();
+const string CurrentPathString = CurrentPath.string();
+
+int example2() {
+    printf("目录为：%s\n", CurrentPathString.c_str());
+    fprintf(stderr, "目录为：%s\n", CurrentPathString.c_str());
+    return 0;
+}
